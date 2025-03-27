8c9ca9a802349352c841cb9243e21ebf0b1531c3
Daniel Rivera <daniel.riverar@pragma.com.co>
2025-03-27 08:12:18 -0500

    catch exceptions on yaml file

--- main.py
+++ main.py
@@ -6,24 +6,25 @@ def generate_uml_class(schema_name, schema) -> str:
     @
     """
     uml = f'class {schema_name} {{\n'
-    
     properties = schema.get('properties', {})
     required_properties = schema.get('required', [])
+
+    relationships = []
     
     for prop, prop_details in properties.items():
         prop_type = prop_details.get('type', 'string')  # Default is string
-        
         # Handle $ref if use nested schemas
         if '$ref' in prop_details:
             ref_schema = prop_details['$ref'].split('/')[-1]  # Extract reference schema's name
             prop_type = ref_schema  # Property type to the referenced schema name
-        
+            relationships.append(f'{schema_name} --> {ref_schema}') 
         # Handle array types
         elif prop_type == 'array':
             items = prop_details.get('items', {})
             if '$ref' in items:
                 ref_schema = items['$ref'].split('/')[-1]
                 prop_type = f'List<{ref_schema}>'
+                relationships.append(f'{schema_name} --> {ref_schema}') 
             elif 'type' in items:
                 prop_type = f'List<{items["type"]}>'
             else:
@@ -33,6 +34,9 @@ def generate_uml_class(schema_name, schema) -> str:
         uml += f'    +{prop_type} {prop}{required_indicator}\n'
     
     uml += '}\n'
+    for relation in relationships:
+        uml += relation + '\n'
+
     return uml
 
 def generate_uml(openapi_spec) -> str:
@@ -51,11 +55,16 @@ file_name="openapi.yaml"
 try:
     with open(file_name, 'r') as file:
         openapi_spec = yaml.safe_load(file)
+        if openapi_spec is None:
+            raise Exception("File or content is not valid ")
 except FileNotFoundError:
     print(f"Error: {file_name}")
     exit()
 except ValueError as e:
     print(f"Error: {e}")
+    exit()
+except Exception as ex:
+    print(f"Error: {ex}")
     exit()
 uml_output = generate_uml(openapi_spec)
 print(uml_output)
